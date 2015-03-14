cb006668f6741164d622c02617961ee1744ba818
Phillip Nientiedt <phillipnientiedt@gmail.com>
2015-03-14 09:22:50 +0000

    engine

--- src/com/base/game/Main.java
+++ src/com/base/game/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		CoreEngine engine;
-			engine = new CoreEngine(800, 600, false, false, 60, new TestGame()); //Window
-//			engine = new CoreEngine(1680, 1050, true, true, 60, new TestGame()); //Fullscreen
+//			engine = new CoreEngine(800, 600, false, false, 60, new TestGame()); //Window
+			engine = new CoreEngine(1680, 1050, true, true, 60, new TestGame()); //Fullscreen
 		engine.createWindow("3D Game Engine");
 		engine.start();
 	}
